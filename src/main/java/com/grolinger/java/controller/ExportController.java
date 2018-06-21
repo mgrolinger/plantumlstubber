ac918d216d5c3b1698c540279af9fe90810442a0
Michael Grolinger <michael.grolinger@m-net.de>
2018-06-21 08:23:36 +0200

    added new application settings and minor improvements with interface aliases

--- src/main/java/com/grolinger/java/controller/ExportController.java
+++ src/main/java/com/grolinger/java/controller/ExportController.java
@@ -2,15 +2,14 @@ package com.grolinger.java.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.grolinger.java.ServiceGenerator;
 import com.grolinger.java.config.Services;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
@@ -26,7 +25,7 @@ import java.util.*;
 @Controller
 public class ExportController {
 
-    private List<Services> servicesList = new LinkedList<>();
+    private List<Services> applicationList = new LinkedList<>();
 
     @Autowired
     private SpringTemplateEngine templateEngine;
@@ -45,10 +44,12 @@ public class ExportController {
         aliasMapper.put("esb", "atlas");
     }
 
+    @SuppressWarnings("squid:S2095")
     @GetMapping("/export")
     public void component(Model model) throws IOException {
 
         // Find all yaml files
+
         Files.list(Paths.get("./target/classes/"))
                 .filter(Files::isRegularFile)
                 .filter(YamlPredicate::isYamlFile)
@@ -58,10 +59,18 @@ public class ExportController {
         String basepath = "./target/";
         String path = "";
         Set<String> dirsCreate = new HashSet<>();
-        for (Services services : servicesList) {
+
+        for (Services services : applicationList) {
+            // Prepare examplefile for every Application
+            StringBuilder exampleFile = new StringBuilder();
+            exampleFile.append("@startuml\n");
+            exampleFile.append("' generated on " + LocalDateTime.now() + "\n");
+            exampleFile.append("'!define DETAILED\n!define UML_STRICT\n\n\n");
+
             Context context = new Context();
             context.setVariable("dateCreated", LocalDateTime.now());
             String applicationName = services.getApplication();
+            applicationName = applicationName.replace('.', '_');
             if (!dirsCreate.contains(applicationName)) {
                 context.setVariable("commonPath", "../");
                 path = basepath + applicationName + "/";
@@ -76,18 +85,15 @@ public class ExportController {
             context.setVariable("connectionColor", colorNameMapper.get(colorName.toLowerCase()));
 
             for (Map.Entry entry : services.getServices().entrySet()) {
+
                 String serviceName = (String) entry.getKey();
+                serviceName = serviceName.replace('.', '_');
                 if (serviceName.equalsIgnoreCase("EMPTY")) {
-                    //serviceName = applicationName;
                     //context.setVariable("serviceName", serviceName);
                     context.setVariable("commonPath", "../");
                     context.setVariable("isRootService", true);
                 } else {
-                    if (applicationName.equalsIgnoreCase("esb")) {
-                        context.setVariable("isRootService", false);
-                    } else {
-                        context.setVariable("isRootService", true);
-                    }
+                    context.setVariable("isRootService", false);
                     context.setVariable("commonPath", "../../");
                     if (!dirsCreate.contains(applicationName + serviceName)) {
                         path = basepath + applicationName + "/" + serviceName + "/";
@@ -99,16 +105,28 @@ public class ExportController {
 
                 String[] interfaces = (String[]) entry.getValue();
                 for (String interfaceName : interfaces) {
+                    interfaceName = interfaceName.replace('.', '_');
                     //interface
                     context.setVariable("interfaceName", interfaceName);
 
                     try (Writer writer = new FileWriter(path + interfaceName + ".iuml")) {
+                        String pEx = (serviceName.equalsIgnoreCase("EMPTY")) ? "" : serviceName + "/";
+                        exampleFile.append("!include " + pEx + interfaceName + ".iuml \n");
+                        String serviceCall = (serviceName.equalsIgnoreCase("EMPTY")) ? applicationName : applicationName+"_"+serviceName;
+                        System.out.println("Export call: "+serviceCall+ "_" + interfaceName + "()");
+                        exampleFile.append(StringUtils.capitalize(serviceCall) + "_" + interfaceName + "()\n\n");
                         writer.write(templateEngine.process("componentExport.html", context));
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    } catch (IOException ignore) {
+                        // do nothing
                     }
                 }
 
+            }
+            exampleFile.append("@enduml");
+            try (Writer writer = new FileWriter(basepath + applicationName + "/" + applicationName + "_example.puml")) {
+                writer.write(exampleFile.toString());
+            } catch (IOException ignore) {
+                // do nothing
             }
         }
     }
@@ -125,9 +143,9 @@ public class ExportController {
         try {
             Services services = mapper.readValue(path.toFile(), Services.class);
             System.out.println(ReflectionToStringBuilder.toString(services, ToStringStyle.MULTI_LINE_STYLE));
-            servicesList.add(services);
-        } catch (Exception e) {
-            e.printStackTrace();
+            applicationList.add(services);
+        } catch (Exception ignore) {
+            //Do nothing
         }
     }
 }
