24522c1f66469c67c3cfe7b511e296488d284b4e
Michael Grolinger <michael.grolinger@m-net.de>
2019-05-16 09:58:52 +0200

    moved scanBasepackage to SpringBoot annotation

--- src/main/java/com/grolinger/java/ServiceGenerator.java
+++ src/main/java/com/grolinger/java/ServiceGenerator.java
@@ -2,10 +2,8 @@ package com.grolinger.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.grolinger.java"})
+@SpringBootApplication(scanBasePackages = {"com.grolinger.java"})
 public class ServiceGenerator {
     public static void main(String[] args) {
         SpringApplication.run(ServiceGenerator.class, args);
