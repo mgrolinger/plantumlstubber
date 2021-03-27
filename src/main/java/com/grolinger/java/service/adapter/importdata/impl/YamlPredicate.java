ab1071c9d46224f4fc472dc478d8d9bf2340e08a
Michael Grolinger <m.grolinger@mailbox.org>
2021-03-27 15:39:55 +0100

    changed old private constructor to lombox annotation

--- src/main/java/com/grolinger/java/service/adapter/importdata/impl/YamlPredicate.java
+++ src/main/java/com/grolinger/java/service/adapter/importdata/impl/YamlPredicate.java
@@ -1,16 +1,17 @@
 package com.grolinger.java.service.adapter.importdata.impl;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.nio.file.Path;
 
 /**
  * Predicate to check if this is a yaml file or not.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 class YamlPredicate {
-    private YamlPredicate() {
-    }
-
     static boolean isYamlFile(Path path) {
-        return path.toAbsolutePath().toString().toLowerCase().contains(".yaml") ||
-                path.toAbsolutePath().toString().toLowerCase().contains(".yml");
+        return path.toAbsolutePath().toString().toLowerCase().endsWith(".yaml") ||
+                path.toAbsolutePath().toString().toLowerCase().endsWith(".yml");
     }
 }
