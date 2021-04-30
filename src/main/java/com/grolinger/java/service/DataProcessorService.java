bc04321b0220a8053d1aeed2e3417fc974d2e95d
Michael Grolinger <m.grolinger@mailbox.org>
2021-04-30 19:18:41 +0200

    corrected imports

--- src/main/java/com/grolinger/java/service/DataProcessorService.java
+++ src/main/java/com/grolinger/java/service/DataProcessorService.java
@@ -2,7 +2,6 @@ package com.grolinger.java.service;
 
 import com.grolinger.java.controller.templatemodel.DiagramType;
 import com.grolinger.java.service.data.ApplicationDefinition;
-import org.thymeleaf.context.Context;
 
 import java.io.IOException;
 import java.util.List;
--- src/main/java/com/grolinger/java/service/impl/DataProcessorServiceImpl.java
+++ src/main/java/com/grolinger/java/service/impl/DataProcessorServiceImpl.java
@@ -13,10 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 @Slf4j
