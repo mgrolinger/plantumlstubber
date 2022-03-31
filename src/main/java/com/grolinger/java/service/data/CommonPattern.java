83e0a6bae02e332742f096970fb1993655a683c6
Michael Grolinger <mgrolinger@users.noreply.github.com>
2022-03-31 21:44:36 +0200

    removed the code because the project moved to codeberg.org, and I don't want any unsecure code lingering around

--- src/main/java/com/grolinger/java/service/data/CommonPattern.java
+++
@@ -1,9 +0,0 @@
-package com.grolinger.java.service.data;
-
-/**
- * Common pattern
- */
-public final class CommonPattern {
-    static final String DOMAIN_DEFINITION_PATTERN = "([<]{2})([a-z]+)([>]{2})";
-    static final String METHOD_PATTERN = "(?=:)((:)([a-z0-9]*))+";
-}
